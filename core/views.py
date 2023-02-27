5d5d43925df7aeb356a891b210fcdf7e1ff88e70
unknown <nicolasgonzalez470@gmail.com>
2023-02-27 02:56:45 -0300

    login page

--- core/views.py
+++ core/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+    else:
+        return render(request, 'signup.html')
