f1bbf2d14b3433d9f518525ee66b95bf01e3c295
unknown <nicolasgonzalez470@gmail.com>
2023-03-04 01:37:05 -0300

    settings - login - logout

--- core/views.py
+++ core/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import *
+from .decorators import unauthorized_user
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
+@login_required(login_url='signin')
+def settings(request):
+    return render(request, 'setting.html')
+
+@unauthorized_user
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -24,18 +32,22 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
-                
+
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
 
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user = user_model, id_user = user_model.id)
                 new_profile.save()
-                return redirect('signin')
+                return redirect('settings')
         else:
             messages.info(request, 'Passwords not matching')
             return redirect('signup') 
     else:
         return render(request, 'signup.html')
-    
+
+@unauthorized_user
 def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -51,3 +63,8 @@ def signin(request):
             return redirect('signin')
     else:
         return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
