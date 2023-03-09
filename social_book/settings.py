2354f87a267bb88b3260ab6d0d393a032d540c1a
unknown <nicolasgonzalez470@gmail.com>
2023-03-09 01:29:02 -0300

    cambios

--- social_book/settings.py
+++ social_book/settings.py
@@ -127,4 +127,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOTS = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
